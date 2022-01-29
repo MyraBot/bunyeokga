97feeb6065c3da42dc621c8c643d33c94c21d2da
Marian <elias-marian@web.de>
2022-01-29 13:16:05 +0100

    :bug: Fixed invalid code

--- src/main/kotlin/com/github/myraBot/bunyeokga/ArgumentBuilder.kt
+++ src/main/kotlin/com/github/myraBot/bunyeokga/ArgumentBuilder.kt
@@ -1,8 +1,22 @@
 package com.github.myraBot.bunyeokga
 
+import com.github.myraBot.diskord.rest.builders.MessageBuilder
+
 class ArgumentBuilder {
     var args = mutableMapOf<String, Any>()
     fun arg(key: String, value: Any) {
         args[key] = value
     }
+}
+
+val externalMap = mutableMapOf<MessageBuilder, suspend ArgumentBuilder.() -> Unit>()
+
+internal var MessageBuilder.arguments: suspend ArgumentBuilder.() -> Unit
+    get() = externalMap[this] ?: {}
+    set(value) {
+        externalMap[this] = value
+    }
+
+fun MessageBuilder.arguments(arguments: suspend ArgumentBuilder.() -> Unit) {
+    this.arguments = arguments
 }
