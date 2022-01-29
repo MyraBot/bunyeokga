d722e20f0c1c2eab4b71246530f4890efbea00b4
Marian <elias-marian@web.de>
2022-01-29 13:17:06 +0100

    :building_construction: Moved arguments to an extension for MessageBuilder

--- src/main/kotlin/com/github/myraBot/bunyeokga/_InteractionMessageBuilder.kt
+++ src/main/kotlin/com/github/myraBot/bunyeokga/_InteractionMessageBuilder.kt
@@ -1,22 +1,16 @@
 package com.github.myraBot.bunyeokga
 
+import com.github.myraBot.diskord.common.entities.guild.Guild
 import com.github.myraBot.diskord.rest.builders.InteractionMessageBuilder
+import com.github.myraBot.diskord.rest.builders.MessageBuilder
 
 
-val externalMap = mutableMapOf<InteractionMessageBuilder, suspend ArgumentBuilder.() -> Unit>()
-
-private var InteractionMessageBuilder.arguments: suspend ArgumentBuilder.() -> Unit
-    get() = externalMap[this] ?: {}
-    set(value) {
-        externalMap[this] = value
-    }
-
-fun InteractionMessageBuilder.arguments(arguments: suspend ArgumentBuilder.() -> Unit) {
-    this.arguments = arguments
+fun InteractionMessageBuilder.get(string: String): String {
+    val lang = this.interaction.guildLocale.value ?: Bunyeokga.defaultLang
+    return lang.get(string)
 }
 
-suspend fun InteractionMessageBuilder.get(string: String): String {
-    val lang = this.interaction.guildLocale.value ?: Bunyeokga.defaultLang
-    return lang.get(string, this.arguments)
+fun MessageBuilder.get(guild: Guild, string: String): String {
+    return guild.locale.get(string)
 }
 
