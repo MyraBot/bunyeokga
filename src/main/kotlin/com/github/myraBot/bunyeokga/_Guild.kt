02aa11f5682aefb47d781eb9d76d39ec0372f7ad
Marian <elias-marian@web.de>
2022-01-29 12:40:34 +0100

    :children_crossing: Added default value to args param of Guild#lang

--- src/main/kotlin/com/github/myraBot/bunyeokga/_Guild.kt
+++ src/main/kotlin/com/github/myraBot/bunyeokga/_Guild.kt
@@ -2,6 +2,6 @@ package com.github.myraBot.bunyeokga
 
 import com.github.myraBot.diskord.common.entities.guild.Guild
 
-suspend fun Guild.lang(string: String, arguments: suspend ArgumentBuilder.() -> Unit): String {
+suspend fun Guild.lang(string: String, arguments: suspend ArgumentBuilder.() -> Unit = {}): String {
     return this.locale.get(string, arguments)
 }
