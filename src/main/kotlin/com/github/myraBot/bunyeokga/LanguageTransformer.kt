820134bddd2641521f804314d28676c45ff763f2
Marian <elias-marian@web.de>
2022-01-29 13:56:18 +0100

    :bug: Fixed argument applying

--- src/main/kotlin/com/github/myraBot/bunyeokga/LanguageTransformer.kt
+++ src/main/kotlin/com/github/myraBot/bunyeokga/LanguageTransformer.kt
@@ -8,8 +8,8 @@ abstract class LanguageTransformer : MessageTransformer {
     override suspend fun onText(builder: MessageBuilder, text: String): String {
         var string = text
         val arguments = ArgumentBuilder().apply { builder.arguments.invoke(this) }
-        arguments.args.forEach { (k, v) -> string = string.replace("{$k}", "$v") }
-        return text
+        arguments.args.forEach { string = string.replace("{$it.key}", "${it.value}") }
+        return string
     }
 
 }
