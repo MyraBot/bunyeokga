1b7208983f4acedba302c9672f8d933c7d0593f8
Marian <elias-marian@web.de>
2022-01-29 13:18:06 +0100

    :coffin: Removed Guild#lang extension

--- src/main/kotlin/com/github/myraBot/bunyeokga/_Guild.kt
+++
@@ -1,7 +0,0 @@
-package com.github.myraBot.bunyeokga
-
-import com.github.myraBot.diskord.common.entities.guild.Guild
-
-suspend fun Guild.lang(string: String, arguments: suspend ArgumentBuilder.() -> Unit = {}): String {
-    return this.locale.get(string, arguments)
-}
