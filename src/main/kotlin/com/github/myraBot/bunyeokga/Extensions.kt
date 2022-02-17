769ca4aab2c12a6947eee1d2e4872cd46d0e08b7
Marian <elias-marian@web.de>
2022-02-17 14:38:02 +0100

    :bug: Fixed stackoverflow error when getting a translation in the argument builder

--- src/main/kotlin/com/github/myraBot/bunyeokga/Extensions.kt
+++ src/main/kotlin/com/github/myraBot/bunyeokga/Extensions.kt
@@ -6,24 +6,30 @@ import com.github.myraBot.diskord.rest.builders.InteractionMessageBuilder
 import com.github.myraBot.diskord.rest.builders.MessageBuilder
 import java.util.*
 
-suspend fun Locale.get(key: String, args: suspend ArgumentBuilder.() -> Unit = {}): String {
-    val locale = Bunyeokga.registeredLanguages[this] ?: getDefaultLanguage()
-    var string = locale[key] as String? ?: getDefaultLanguage()[key] as String? ?: throw IllegalStateException("Cannot find $key")
-    val arguments = ArgumentBuilder().apply { args.invoke(this) }
-    arguments.args.forEach {
+suspend fun Locale.get(key: String, args: (suspend ArgumentBuilder.() -> Unit)? = null): String {
+    val translations: Properties = Bunyeokga.registeredLanguages[this] ?: getDefaultLanguage()
+    var string = translations[key] as String? ?: getDefaultLanguage()[key] as String? ?: throw IllegalStateException("Cannot find $key")
+
+    val arguments: MutableMap<String, Any> = args?.let {
+        ArgumentBuilder().apply { args.invoke(this) }.args
+    } ?: mutableMapOf()
+    arguments.forEach {
         string = string.replace("{${it.key}}", it.value.toString())
     }
+
     return string
 }
 
-suspend fun Guild.lang(key: String, args: suspend ArgumentBuilder.() -> Unit = {}): String = this.locale.get(key, args)
+suspend fun Guild.lang(key: String, args: suspend ArgumentBuilder.() -> Unit): String = this.locale.get(key, args)
 
 suspend fun InteractionMessageBuilder.get(key: String): String {
-    val lang = this.interaction.guildLocale.value ?: Bunyeokga.defaultLang
+    val lang: Locale = this.interaction.guildLocale.value ?: Bunyeokga.defaultLang
     return lang.get(key, this.arguments)
 }
 
 suspend fun MessageBuilder.get(guild: Guild, key: String): String = guild.locale.get(key, this.arguments)
+
+suspend fun ArgumentBuilder.get(guild: Guild, key: String): String = guild.locale.get(key)
 
 private fun getDefaultLanguage(): Properties = Bunyeokga.registeredLanguages[Bunyeokga.defaultLang]
     ?: throw IllegalStateException("The default language file is missing")
